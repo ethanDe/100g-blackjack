57f2ea0e7a9125137db7cb542811f11928a58fbd
ethanDe <ethand971@deltalearns.ca>
2022-01-14 10:25:36 -0800

    In progress

--- x01_deck.py
+++ x01_deck.py
@@ -5,7 +5,7 @@ Create a list of cards
 the cards should be denoted with a number or A, J, Q, K, T (for ace, jack, queen, king or ten)
 as well as a suit
 """
-
+import random as r
 def createDeck():
   ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
   suits = ['C','D','H','S']
--- x03_bust.py
+++ x03_bust.py
@@ -14,8 +14,11 @@ def busts(score):
   True : user busts if the score is over 21
   False : user does not bust becuase score is 21 or less
   '''
-  
-  return None
+  x = int(score)
+  if x>21:
+    return True
+  else:
+    return False
 
 
 def main():
