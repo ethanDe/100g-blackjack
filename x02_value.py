900fd0f1810af4627739b2e1bb6885e56f23ddef
ethanDe <ethand971@deltalearns.ca>
2022-01-17 14:38:32 -0800

    In progress

--- x02_value.py
+++ x02_value.py
@@ -9,17 +9,28 @@ def value(hand):
   return:
   int the total value of the hand
   may return a list if the hand contains an Ace
-  eg:
+  eg: [8,18]
   '''
-  
-  return None
+  value = []
+  face = ['J','K','Q','10']
+  no = ['1','2','3','4','5','6','7','8','9']
+  ex = ['A']
+  for i in hand:
+    if value[:1] in face:
+      return int(10)
+    elif value[:1] in no:
+      return int(value[:1])
+    elif value[:1] in ex:
+      return int(1) and int(11)
 
 
+
+exit()
 def main():
   assert value(['AH','3D','4S']) ==[8,18]
   assert value(['KH','TD']) == 20
   assert value(['3D','8H']) == 11
   assert value(['KC','6S','QD']) == 26
 
-if __name__ == "__name__":
+if __name__ == "__main__":
   main()
